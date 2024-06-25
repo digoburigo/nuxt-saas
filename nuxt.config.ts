194c0b4d9a04f5b8b59798578cbe8502eeb80156
digoburigo <rodrigobesmeraldino@gmail.com>
2024-06-25 15:59:49 -0300

    build(deps): update pkgs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
   },
   nitro: {
     // preset: 'cloudflare-pages',
-    // preset: 'bun',
+    preset: 'bun',
     prerender: {
       autoSubfolderIndex: false,
     },
